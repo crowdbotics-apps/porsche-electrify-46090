113c2b3795462d05a1b183db185b4203358555a2
Crowdbotics <team@crowdbotics.com>
2024-02-07 17:16:55 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "porsche-electrify-46090",
+  url: "https://porsche-electrify-46090.botics.co",
+  api: "https://porsche-electrify-46090.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
